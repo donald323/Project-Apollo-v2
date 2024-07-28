0d85a54b4f038fde900ec0d766c04871496fd727
donald323 <117969351+donald323@users.noreply.github.com>
2024-07-28 10:15:02 +0800

    Initial Attemptfor SAC

---
+++ DRL_training.py
@@ -0,0 +1,8 @@
+from stable_baselines3 import SAC
+import earth_moon_simulator as ems
+
+env = ems.earth_moon_env()
+
+model = SAC("MlpPolicy",env,verbose=1)
+model.learn(total_timesteps = int(1e9),log_interval = 10)
+model.save("EMS_SAC")
